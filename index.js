b9b4be46780fcee3851bdf19f32e9306396048a6
tatra-t <tanechka.dush@gmail.com>
2023-04-05 00:03:13 +0300

    add calculation

--- index.js
+++ index.js
@@ -1,14 +1,5 @@
 'use strict';
 
-function viewResultField(){
-  viewResult.style.display = "block";
-};
-
-
-
-
-
-
 let inputStart = document.querySelector(".inputStart");
 let inputEnd = document.querySelector(".inputEnd");
 let inputSelectedDays = document.querySelector(".selectedDays"); 
@@ -16,64 +7,93 @@ let inputDimension = document.querySelector(".dimension");
 let inputPreset = document.querySelector(".preset")
 let calculate = document.querySelector(".calculate");
 let viewResult = document.querySelector(".viewResult");
-//viewResult.innerHTML = `RESULT: ${currentResult}`;
 
 calculate.addEventListener("click", () => {
- let start = Date.parse(inputStart.value);
-  console.log(start);
-  return start;
-})
-calculate.addEventListener("click", () => {
- let end = Date.parse(inputEnd.value);
-  console.log(end);
-  return end;
-})
-calculate.addEventListener("click", () => {
- let selectedDays = inputSelectedDays.value;
-  console.log(selectedDays);
-  return selectedDays;
-})
-calculate.addEventListener("click", () => {
- let dimension = inputDimension.value;
-  console.log(dimension);
-  return dimension;
-})
-
-console.log(start);
-
-calculate.addEventListener('click', viewResultField);// появляется поле резалт
-calculate.addEventListener('click', durationBetweenDates);
-function durationBetweenDates(
-  start,
-  end,
-  dimension
-) {
-  const DAY_IN_MILLISECONDS = 24 * 60 * 60 * 1000;
-  const HOUR_IN_MILLISECONDS = 60 * 60 * 1000;
-  const MIN_IN_MILLISECONDS = 60 * 1000;
-  const SEC_IN_MILLISECONDS = 1000;
-  const startDate = Date.parse(start);
-  const endDate = Date.parse(end);
-  let resultMillisec = Math.abs(startDate - endDate);
+  let start = Date.parse(inputStart.value);
+  let end = Date.parse(inputEnd.value);
+  let selectedDays = inputSelectedDays.value;
+  let dimension = inputDimension.value;
+  let resultMillisec = end - start;
   let result;
-  
-    console.log(resultMillisec);
-  switch (dimension) {
-    case "4":
-      result = resultMillisec / SEC_IN_MILLISECONDS;
-      break;
-    case "3":
-      result = resultMillisec / MIN_IN_MILLISECONDS;
-      break;
-    case "2":
-      result = resultMillisec / HOUR_IN_MILLISECONDS;
-      break;
-    case "1":
-      result = resultMillisec / DAY_IN_MILLISECONDS;
-      break;
+  if (selectedDays === "1") {
+    switch (dimension) {
+      case "4":
+        result = `${resultMillisec / 1000 +86400} SECONDS`;
+        break;
+      case "3":
+        result = `${resultMillisec / 60000+1440} MINUTS`;
+        break;
+      case "2":
+        result = `${resultMillisec / 3600000 +24} HOURS`;
+        break;
+      case "1":
+        result = `${resultMillisec / 86400000 +1} DAYS`;
+        break;
+    }
+    viewResult.innerHTML = `RESULT: ${result}`;
+    //return result;
   }
-  let currentResult = `${result} ${dimension}`;
-    console.log(currentResult);
-    return currentResult;
-    
-}
+  if (selectedDays === "3") {
+    let resultDay = resultMillisec / 86400000;
+    let weekend = 0;
+    let currentlyDay;
+    let getday = new Date();
+    for (let i = 0; i <= resultDay; i++) {
+      getday.setTime(start + 86400000 * i);
+      currentlyDay = getday.getDay();
+      if (currentlyDay === 0 || currentlyDay === 6) {
+        weekend++;
+      }
+    }
+    switch (dimension) {
+      case "4":
+        result = `${weekend * 86400} SECONDS`;
+        break;
+      case "3":
+        result = `${weekend * 1440} MINUTS`;
+        break;
+      case "2":
+        result = `${weekend * 24} HOURS`;
+        break;
+      case "1":
+        result = `${weekend} DAYS`;
+        break;
+    }
+    viewResult.innerHTML = `RESULT: ${result}`;
+    //return result;
+  }
+  if (selectedDays === "2") {
+    let resultDay = resultMillisec / 86400000;
+    let weekday = 0;
+    let currentlyDay;
+    let getday = new Date();
+    for (let i = 0; i <= resultDay; i++) {
+      getday.setTime(start + 86400000 * i);
+      currentlyDay = getday.getDay();
+      if (currentlyDay === 1 || currentlyDay === 2 || currentlyDay === 3|| currentlyDay === 4|| currentlyDay === 5) {
+        weekday++;
+      }
+    }
+    switch (dimension) {
+      case "4":
+        result = `${weekday * 86400} SECONDS`;
+        break;
+      case "3":
+        result = `${weekday * 1440} MINUTS`;
+        break;
+      case "2":
+        result = `${weekday * 24} HOURS`;
+        break;
+      case "1":
+        result = `${weekday} DAYS`;
+        break;
+    }
+    viewResult.innerHTML = `RESULT: ${result}`;
+   // return result;
+  }
+})
+
+function viewResultField(){
+  viewResult.style.display = "block";
+};
+calculate.addEventListener('click', viewResultField);// появляется поле резалт
