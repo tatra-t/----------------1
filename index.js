14fb3655f8b771c33ecef94b97cccae2f7804b3a
tatra-t <tanechka.dush@gmail.com>
2023-04-17 18:47:07 +0300

    result table

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-'use strict';
+"use strict";
 const DAY_IN_MILLISECONDS = 24 * 60 * 60 * 1000;
 const HOUR_IN_MILLISECONDS = 60 * 60 * 1000;
 const MIN_IN_MILLISECONDS = 60 * 1000;
 const SEC_IN_MILLISECONDS = 1000;
 let inputStart = document.querySelector(".inputStart");
 let inputEnd = document.querySelector(".inputEnd");
-let inputSelectedDays = document.querySelector(".selectedDays"); 
+let inputSelectedDays = document.querySelector(".selectedDays");
 let inputDimension = document.querySelector(".dimension");
-let inputPreset = document.querySelector(".preset")
+let inputPreset = document.querySelector(".preset");
 let calculate = document.querySelector(".calculate");
 let viewResult = document.querySelector(".viewResult");
 let startForLocal = document.querySelector(".startLocal");
@@ -15,28 +15,30 @@ let endForLocal = document.querySelector(".endLocal");
 let resultForLocal = document.querySelector(".resultLocal");
 let tableLocal = document.querySelector(".tableLocal");
 let dateEnd = document.querySelector(".dateEnd");
-let persetWeek = document.querySelector('#week');
+let persetWeek = document.querySelector("#week");
 let persetMonth = document.querySelector("#month");
-let persetNone = document.querySelector('#none');
-let clear = document.querySelector('.clear');
+let persetNone = document.querySelector("#none");
+let clear = document.querySelector(".clear");
 let dimension = inputDimension.value;
 let result;
 let resultData = [];
 let j = 0;
 
-if (localStorage.getItem('result') !== null) {
-  resultData = JSON.parse(localStorage.getItem('result'));
+if (localStorage.getItem("result") !== null) {
+  resultData = JSON.parse(localStorage.getItem("result"));
   for (j = 0; j < resultData.length; j++) {
     let newline = document.createElement("tr");
     newline.className = `${j}`;
-    newline.innerHTML = `<th scope="row">${j + 1}</th> <td>${resultData[j].startStorage}</td><td>${resultData[j].endStorage}</td><td>${resultData[j].result}</td>`;
+    newline.innerHTML = `<th scope="row">${j + 1}</th> <td>${
+      resultData[j].startStorage
+    }</td><td>${resultData[j].endStorage}</td><td>${resultData[j].result}</td>`;
     tableLocal.prepend(newline);
-  } 
-} 
+  }
+}
 
 inputStart.addEventListener("change", () => {
   inputEnd.disabled = false;
-})
+});
 inputEnd.addEventListener("change", () => {
   if (Date.parse(inputEnd.value) < Date.parse(inputStart.value)) {
     calculate.disabled = true;
@@ -45,31 +47,69 @@ inputEnd.addEventListener("change", () => {
     calculate.disabled = false;
     dateEnd.style.display = "none";
   }
-})
+});
 persetWeek.addEventListener("change", () => {
   let d = new Date(inputStart.value);
   let inputEndTemp = new Date(d.setDate(d.getDate() + 7));
   inputEnd.value = formatDate(inputEndTemp);
-  console.log("inputEnd", inputEndTemp)
-})
+  console.log("inputEnd", inputEndTemp);
+});
 persetMonth.addEventListener("change", () => {
   let d = new Date(inputStart.value);
   let inputEndTemp = new Date(d.setMonth(d.getMonth() + 1));
   inputEnd.value = formatDate(inputEndTemp);
   console.log(inputEnd.value);
-})
+});
 persetNone.addEventListener("change", () => {
   inputEnd.value = "";
-})
+});
 
 if (inputStart.value === "" || inputEnd.value === "") {
   calculate.disabled = true;
 }
-clear.addEventListener('click', () => {
-  inputStart.value ="";
+clear.addEventListener("click", () => {
+  inputStart.value = "";
   inputEnd.value = "";
   inputEnd.disabled = true;
-})
+});
+
+// Функція яка буде малювати таблицю
+const renderHistoryTable = () => {
+  const resultData = JSON.parse(localStorage.getItem("result"));
+
+  // Очищуємо попередні результати таблиці
+  tableLocal.innerHTML = "";
+
+  // Перебераємо усі елементи які є у localStorage
+  resultData.forEach((resultObj, index) => {
+    // Створюємо кожний рядок таблиці для кожного з елементів
+    let newline = document.createElement("tr");
+    newline.className = index;
+    newline.innerHTML = `<th scope="row">${index + 1}</th> <td>${
+      resultObj.startStorage}</td><td>${resultObj.endStorage}</td><td>${resultObj.result}</td>`;
+    tableLocal.prepend(newline);
+  });
+};
+
+// Функція для збереження нового елементу у масив localStorage
+const storeResultInLocalStorage = (result) => {
+  const resultData = JSON.parse(localStorage.getItem("result")) || [];
+
+  // Перевіряємо чи вже десять елементів у localStorage
+  if (resultData.length >= 10) {
+    // Видаляємо останній
+    resultData.shift();
+  }
+
+  resultData.push({
+    startStorage: inputStart.value,
+    endStorage: inputEnd.value,
+    result: result
+  });
+
+  // Зберігаємо оновлений масив у localStorage
+  localStorage.setItem("result", JSON.stringify(resultData));
+};
 
 calculate.addEventListener("click", () => {
   let start = Date.parse(inputStart.value);
@@ -119,57 +159,44 @@ calculate.addEventListener("click", () => {
     for (let i = 0; i <= resultDay; i++) {
       getday.setTime(start + 86400000 * i);
       currentlyDay = getday.getDay();
-      if (currentlyDay === 1 || currentlyDay === 2 || currentlyDay === 3|| currentlyDay === 4|| currentlyDay === 5) {
+      if (
+        currentlyDay === 1 ||
+        currentlyDay === 2 ||
+        currentlyDay === 3 ||
+        currentlyDay === 4 ||
+        currentlyDay === 5
+      ) {
         day++;
       }
     }
     result = convertTime(day);
     viewResult.innerHTML = `RESULT: ${result}`;
   }
-  let resultStorage = {
-    startStorage: inputStart.value,
-    endStorage: inputEnd.value,
-    result: result,
-  }
-  
-  if (resultData.length >= 10) {
-    resultData.shift();
-  }
-  resultData.push(resultStorage);
 
-  localStorage.setItem("result", JSON.stringify(resultData));
-  console.log(resultData);
-  console.log();
-  let newLineData = JSON.parse(localStorage.getItem('result'));
-  if (result.length <= 10) {
-    let newline = document.createElement("tr");
-    newline.className = `${j}`;
-    newline.innerHTML = `<th scope="row">${j + 1}</th> <td>${newLineData[j].startStorage}</td><td>${newLineData[j].endStorage}</td><td>${newLineData[j].result}</td>`;
-    tableLocal.prepend(newline);
-    j++;
-  } else {
-    
-  }
-  
-})
+  // Збереженно у localStorage та малювання таблиці
+  // 1. Зберігти дані у localStorage і у цьому пункті ще і перевірити чи вже досягнений ліміт у 10 результатів
+  storeResultInLocalStorage(result);
+  // 2. Показати користувачу таблицу, у якій буде братися актуальний список з localStorage
+  renderHistoryTable();
+});
 
 function viewResultField() {
   viewResult.style.display = "block";
-};
-calculate.addEventListener('click', viewResultField);// появляется поле резалт
+}
+calculate.addEventListener("click", viewResultField); // появляется поле резалт
 
 function formatDate(inputEndTemp) {
   const date = new Date(inputEndTemp);
   let DD = date.getDate();
-   if (DD <10) { 
-    DD = "0"+ DD;
+  if (DD < 10) {
+    DD = "0" + DD;
   }
   let MO = date.getMonth() + 1;
-  if (MO <10) { 
-    MO = "0"+ MO;
+  if (MO < 10) {
+    MO = "0" + MO;
   }
-  let YYYY= date.getFullYear();
-  let inputEnd = YYYY + "-" + MO + "-" + DD ;
+  let YYYY = date.getFullYear();
+  let inputEnd = YYYY + "-" + MO + "-" + DD;
   return inputEnd;
 }
 
@@ -191,11 +218,3 @@ function convertTime(day) {
   console.log(result);
   return result;
 }
-
-
-/*if (j >= 11) {
-    let removeline = document.getElementsByClassName(j-11);
-    for (let k = 0; k <= removeline.length; k++) {
-    removeline[k].remove();
-     }
-  }*/
